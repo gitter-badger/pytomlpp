98a429b2bb821bb960fd24aa88feefe5f772021e
Bob Fang <bob.fang.london@gmail.com>
2020-06-22 22:24:10 +0000

    Add encoder error test

--- tests/python-tests/test_api.py
+++ tests/python-tests/test_api.py
@@ -99,3 +99,11 @@ def test_invalid_toml_files(toml_file):
         with open(str(toml_file), "r") as f:
             toml_file_string = f.read()
             pytomlpp.loads(toml_file_string)
+
+
+class A:
+    pass
+
+def test_invalid_encode():
+    with pytest.raises(pytomlpp.EncodeError):
+        pytomlpp.dumps({'a': A()})
